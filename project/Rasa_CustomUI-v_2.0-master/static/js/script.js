7bc9ccd98174e6a72de6c1110e89e225820d8092
PRAkamble <prashant7kamble@gmail.com>
2020-09-15 17:12:22 +0530

    First commit to generate unique conversational id for rasa

---
+++ project/Rasa_CustomUI-v_2.0-master/static/js/script.js
@@ -0,0 +1,170 @@
+// on input/text enter--------------------------------------------------------------------------------------
+$('.usrInput').on('keyup keypress', function (e) {
+	var keyCode = e.keyCode || e.which;
+	var text = $(".usrInput").val();
+	if (keyCode === 13) {
+		if (text == "" || $.trim(text) == '') {
+			e.preventDefault();
+			return false;
+		} else {
+			$(".usrInput").blur();
+			setUserResponse(text);
+			send(text);
+			e.preventDefault();
+			return false;
+		}
+	}
+});
+
+
+//------------------------------------- Set user response------------------------------------
+function setUserResponse(val) {
+
+
+	var UserResponse = '<img class="userAvatar" src=' + "./static/img/userAvatar.jpg" + '><p class="userMsg">' + val + ' </p><div class="clearfix"></div>';
+	$(UserResponse).appendTo('.chats').show('slow');
+	$(".usrInput").val('');
+	scrollToBottomOfResults();
+	$('.suggestions').remove();
+}
+
+//---------------------------------- Scroll to the bottom of the chats-------------------------------
+function scrollToBottomOfResults() {
+	var terminalResultsDiv = document.getElementById('chats');
+	terminalResultsDiv.scrollTop = terminalResultsDiv.scrollHeight;
+}
+
+
+sessionStorage.setItem("conversation-id",create_UUID());
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+	});
+	console.log(uuid);
+	return uuid;
+}
+
+function send(message) {
+	console.log("User Message:", message);
+	$.ajax({
+		url: 'http://localhost:5005/webhooks/rest/webhook',
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify({
+			"message": message,
+			"sender": sessionStorage.getItem("conversation-id")
+		}),
+		success: function (data, textStatus) {
+			setBotResponse(data);
+			console.log("Rasa Response: ", data, "\n Status:", textStatus)
+		},
+		error: function (errorMessage) {
+			setBotResponse("");
+			console.log('Error' + errorMessage);
+
+		}
+	});
+}
+
+//------------------------------------ Set bot response -------------------------------------
+function setBotResponse(val) {
+	setTimeout(function () {
+		if (val.length < 1) {
+			//if there is no response from Rasa
+			msg = 'I couldn\'t get that. Let\' try something else!';
+
+			var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + msg + '</p><div class="clearfix"></div>';
+			$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+
+		} else {
+			//if we get response from Rasa
+			for (i = 0; i < val.length; i++) {
+				//check if there is text message
+				if (val[i].hasOwnProperty("text")) {
+					var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + val[i].text + '</p><div class="clearfix"></div>';
+					$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+				}
+
+				//check if there is image
+				if (val[i].hasOwnProperty("image")) {
+					var BotResponse = '<div class="singleCard">' +
+						'<img class="imgcard" src="' + val[i].image + '">' +
+						'</div><div class="clearfix">'
+					$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+				}
+
+				//check if there is  button message
+				if (val[i].hasOwnProperty("buttons")) {
+					addSuggestion(val[i].buttons);
+				}
+
+			}
+			scrollToBottomOfResults();
+		}
+
+	}, 500);
+}
+
+
+// ------------------------------------------ Toggle chatbot -----------------------------------------------
+$('#profile_div').click(function () {
+	$('.profile_div').toggle();
+	$('.widget').toggle();
+	scrollToBottomOfResults();
+});
+
+$('#close').click(function () {
+	$('.profile_div').toggle();
+	$('.widget').toggle();
+});
+
+
+// ------------------------------------------ Suggestions -----------------------------------------------
+
+function addSuggestion(textToAdd) {
+	setTimeout(function () {
+		var suggestions = textToAdd;
+		var suggLength = textToAdd.length;
+		$(' <div class="singleCard"> <div class="suggestions"><div class="menu"></div></div></diV>').appendTo('.chats').hide().fadeIn(1000);
+		// Loop through suggestions
+		window.btnObj = [];
+		for (i = 0; i < suggLength; i++) {
+			console.log('suggestions[i].title '+suggestions[i].title);
+			console.log('suggestions[i].payload '+suggestions[i].payload);
+			window.btnObj[i] = {
+				'title':suggestions[i].title,
+				'payload' : suggestions[i].payload
+			};
+			$('<div class="menuChips">' + suggestions[i].title + '</div>').appendTo('.menu');
+		}
+		scrollToBottomOfResults();
+	}, 1000);
+}
+
+
+// on click of suggestions, get the value and send to rasa
+$(document).on("click", ".menu .menuChips", function () {
+	var len = window.btnObj.length;
+	var text = "";
+	var payload = "";
+	for(i = 0;i<len;i++){
+
+		if(window.btnObj[i].title == this.innerText){
+			console.log('title '+window.btnObj[i].title);
+			console.log('payload '+window.btnObj[i].payload);
+			console.log('this.innerText '+this.innerText);
+			text = window.btnObj[i].title;
+			payload = window.btnObj[i].payload;
+		}
+
+	}
+
+	var text = this.innerText;
+	setUserResponse(text);
+	send(payload);
+	$('.suggestions').remove(); //delete the suggestions 
+});
